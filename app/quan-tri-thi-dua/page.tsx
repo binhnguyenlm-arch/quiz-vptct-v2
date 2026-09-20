802b26f2dce028a3c19f986fd51a8635fa3bc33e
binhnguyenlm-arch <binhnguyenlm@gmail.com>
2026-09-20 08:08:55 +0700

    Create page.tsx

---
+++ app/quan-tri-thi-dua/page.tsx
@@ -0,0 +1,9 @@
+import Commendations from '../../components/Commendations';
+
+export const metadata = {
+  title: 'Quản trị thi đua - Khen thưởng | Văn phòng Tổng công ty'
+};
+
+export default function Page() {
+  return <Commendations manage />;
+}
